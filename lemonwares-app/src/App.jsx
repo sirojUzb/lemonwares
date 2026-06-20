d6b629b88d0ca754f273643c8adfc960052814a7
sirojUz <sirojiddinmamatovuzb@gmail.com>
2026-06-20 22:45:54 +0900

    lemonwaresBody

---
+++ lemonwares-app/src/App.jsx
@@ -0,0 +1,27 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import CloudHosting from "./components/CloudHosting";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import PricingCards from "./components/PricingCards";
+import Testimonials from "./components/Testimonials";
+import Brands from "./components/Brands";
+import CtaBanner from "./components/CtaBanner";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <Hero />
+      <CloudHosting />
+      <Services />
+      <Pricing />
+      <PricingCards />
+      <Testimonials />
+      <Brands />
+      <CtaBanner />
+    </div>
+  );
+}
+
+export default App;
